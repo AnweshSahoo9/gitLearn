24160875c51e843206581f33a5ea815981deaa12
Anwesh Sahoo <anwesh.sahoo@gmail.com>
2021-11-11 12:23:56 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
     public static void main(String[] args){
         int a=78;
         int b=87;
-        System.out.println(a+b);
-        
+        System.out.println("The sum is: "+(a+b));
+
     }
 }
