d6c0fac34f82838acb816c2f225c3b18da11b0b1
Anwesh Sahoo <anwesh.sahoo@gmail.com>
2021-11-11 13:12:50 +0530

    adding implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,6 @@ public class Diff {
     public static void main(String[] args){
         int d=76;
         int c=21;
-        System.out.println(d-c);
+        System.out.println("The diff is: "+(d-c));
     }
 }
